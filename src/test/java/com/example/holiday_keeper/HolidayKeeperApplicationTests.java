8ea07e71288b41b4985fec1b9810ae9ac819326c
mystagogy <hi7747@naver.com>
2025-07-18 01:06:29 +0900

    Create Project

---
+++ src/test/java/com/example/holiday_keeper/HolidayKeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.holiday_keeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolidayKeeperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
